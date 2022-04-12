ce6c2d8975224911f96481627d1461975ea4f998
Erick Hernandez <takashi.onimaru@gmail.com>
2022-04-12 13:07:14 -0400

    Componentes creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PersonajesComponent } from './components/personajes/personajes.component';
+import { ProfesoresComponent } from './components/profesores/profesores.component';
+import { EstudiantesComponent } from './components/estudiantes/estudiantes.component';
+import { ListadoComponent } from './components/listado/listado.component';
+import { SolicitudesComponent } from './components/solicitudes/solicitudes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonajesComponent,
+    ProfesoresComponent,
+    EstudiantesComponent,
+    ListadoComponent,
+    SolicitudesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
