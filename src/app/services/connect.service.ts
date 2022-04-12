100ff9eeee4665b174c3cbee904be541c6c1184c
Erick Hernandez <takashi.onimaru@gmail.com>
2022-04-12 13:34:59 -0400

    Servicios creados

--- src/app/services/connect.service.ts
+++ src/app/services/connect.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConnectService {
+  APIHP: string = 'https://hp-api.herokuapp.com/api/characters/';
 
-  constructor() { }
+  constructor(private clientService:HttpClient) { }
+
+  ObtenerPersonajes(casa:string):Observable<any>{
+    return this.clientService.get(this.APIHP+'house/'+casa);
+  }
+
+  ObtenerProfesores():Observable<any>{
+    return this.clientService.get(this.APIHP+'staff');
+  }
+
+  ObtenerEstudiantes():Observable<any>{
+    return this.clientService.get(this.APIHP+'students');
+  }
 }
--- src/app/services/requests.service.ts
+++ src/app/services/requests.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { Request } from '../models/Requests';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestsService {
 
+  Solicitudes!: Request[];
+
   constructor() { }
+
+  getSolicitud() {
+    const local: any = localStorage.getItem('solicitud');
+    if(local === null) {
+      this.Solicitudes = [];
+    } else {
+      this.Solicitudes = JSON.parse(local);
+    }
+    return this.Solicitudes;
+  }
+
+  addSolicitud(newSolicitud: Request) {
+    this.Solicitudes.push(newSolicitud);
+    let solicitud = [];
+    const local: any = localStorage.getItem('solicitud');
+    if(local === null) {
+      solicitud = [];
+      solicitud.push(newSolicitud);
+      localStorage.setItem('solicitud', JSON.stringify(solicitud));
+    } else {
+      solicitud = JSON.parse(local);
+      solicitud.push(newSolicitud); 
+      localStorage.setItem('solicitud', JSON.stringify(solicitud));
+    }
+  }
+
+  deleteSolicitud(delSolicitud: Request) {
+    for (let i = 0; i < this.Solicitudes.length; i++) {
+      if (delSolicitud == this.Solicitudes[i]) {
+        this.Solicitudes.splice(i, 1);
+        localStorage.setItem('solicitud', JSON.stringify(this.Solicitudes));
+      }
+    }
+  }
 }
