9a9b909840ac17643f50e3dcf923dbc499e1e115
Erick Hernandez <takashi.onimaru@gmail.com>
2022-04-12 13:54:05 -0400

    Estudiantes funcionando

--- src/app/components/estudiantes/estudiantes.component.ts
+++ src/app/components/estudiantes/estudiantes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ConnectService } from 'src/app/services/connect.service';
+import { DataPj } from 'src/app/models/DataPj';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-estudiantes',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./estudiantes.component.css']
 })
 export class EstudiantesComponent implements OnInit {
+  dataSource:any;
+  isLoading:Boolean = true;
 
-  constructor() { }
+  constructor(private connectService: ConnectService) { }
 
   ngOnInit(): void {
+    this.isLoading = true;
+    this.connectService.ObtenerEstudiantes().subscribe(resp => {
+      this.dataSource = new MatTableDataSource<DataPj>(resp);
+      this.isLoading = false;
+    });
   }
 
 }
